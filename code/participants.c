b54a6fec20b7430d3f7517c299de770d8e27a3bf
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2014-01-23 09:45:23 -0600

    Add simple C program.

---
+++ code/participants.c
@@ -0,0 +1,25 @@
+/**
+ * participants.c
+ *   A simple C program to print the participants from the class.
+ *   Intended as an example of updating code on GitHub.
+ */
+
+// +---------+---------------------------------------------------------
+// | Headers |
+// +---------+
+
+#include <stdio.h>
+
+// +------+------------------------------------------------------------
+// | Main |
+// +------+
+
+int 
+main (int argc, char *argv[])
+{
+  int i = 0;
+  printf ("%02d: Samuel A. Rebelsky\n", ++i);
+  printf ("%02d: Sumee Zahid\n", ++i);
+  printf ("There are %i participants.\n", i);
+  return 0;
+} // main
